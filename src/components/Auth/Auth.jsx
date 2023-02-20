2ca6d2130e7546db38e8024ca533f39d09b213c7
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-02-20 18:52:17 -0300

    first commit

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LoginButton from '../blueprints/buttonsAuth/LoginButton'
+
+import Profile from '../blueprints/buttonsAuth/Perfil'
+const Auth = () => {
+  return (
+    <div>
+      <LoginButton />
+
+      <Profile />
+    </div>
+  )
+}
+
+export default Auth
