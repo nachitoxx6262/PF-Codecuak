8480d9a82d65f00002e67e94b484a2f7aed3ba6f
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-02-26 15:36:48 -0300

    fix form

--- src/axiosFunctions.js
+++ src/axiosFunctions.js
@@ -1,64 +1,80 @@
 import axios from "axios";
-const URL_BASE = "https://backend-production-c946.up.railway.app"
+const URL_BASE = "https://backend-production-c946.up.railway.app";
 
 // RUTA PARA PUBLICAR POST
 // falta fixear las rutas
 export const sendPost = async (content, userId, token) => {
-  let response = await axios.post(`${URL_BASE}/socialcuak`, { content, userId }, { headers: { 'x-auth-token': token } });
-  return response
-}
-
+  let response = await axios.post(
+    `${URL_BASE}/socialcuak`,
+    { content, userId },
+    { headers: { "x-auth-token": token } }
+  );
+  return response;
+};
 
 // RUTA PUBLICAR COMENTARIOS
 
 export const sendComment = async (content, userId, postId) => {
-  let data = await axios.post(`${URL_BASE}/socialcuak/${postId}/comment`, { content, userId }, );
-  return data
+  let data = await axios.post(`${URL_BASE}/socialcuak/${postId}/comment`, {
+    content,
+    userId,
+  });
+  return data;
 };
 
 // RUTA PARA EDITAR UN POST
 
 export const editPost = async (content, id) => {
   try {
-    const data = await axios.put(`${URL_BASE}/socialcuak/${id}`, {content});
-    return "Publicacion modificada con exito"
+    const data = await axios.put(`${URL_BASE}/socialcuak/${id}`, { content });
+    return "Publicacion modificada con exito";
   } catch (error) {
-    console.log(error.mesage)
-    return "Algo ha salido mal"
+    console.log(error.mesage);
+    return "Algo ha salido mal";
   }
-} 
+};
 // RUTA PARA BORRAR UN POST
 
 export const deletePost = async (id) => {
   try {
-    const data = await axios.delete(`${URL_BASE}/socialcuak/${id}`)
-    return "Se ha eliminado la publicacion"
+    const data = await axios.delete(`${URL_BASE}/socialcuak/${id}`);
+    return "Se ha eliminado la publicacion";
   } catch (error) {
-    console.log(error.message)
-    return "Algo ha salido mal"
+    console.log(error.message);
+    return "Algo ha salido mal";
   }
-}
+};
 // RUTA POST DEL MERCADO PAGO
-export const sendMP = async (donacion,input) =>{
-  let data = await axios.post(`${URL_BASE}/payment`,{...donacion,...input})
+export const sendMP = async (donacion, input) => {
+  let data = await axios.post(`${URL_BASE}/payment`, { ...donacion, ...input });
   console.log(data.data);
-  return window.location.href=data.data
-}
-  // RUTA POST DEL USUARIO REGISTRADO
-  
-  export const userRegister = async (name,email,nickName,password)=>{
+  return (window.location.href = data.data);
+};
+// RUTA POST DEL USUARIO REGISTRADO
 
-    let response = await axios.post(`${URL_BASE}/auth/signup`,{name,email,nickName,password})
-    return response
-  }
-  // RUTA POST DEL LOG IN DE USUARIOS
-
-export const userLogin = async (email, password)=>{
-  console.log(email,password)
+export const userRegister = async (name, email, nickName, password) => {
   try {
-    let response = await axios.post(`${URL_BASE}/auth/login`,{email,password})
-    return response.data
+    let response = await axios.post(`${URL_BASE}/auth/signup`, {
+      name,
+      email,
+      nickName,
+      password,
+    });
+    return response;
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
+// RUTA POST DEL LOG IN DE USUARIOS
+
+export const userLogin = async (email, password) => {
+  try {
+    let response = await axios.post(`${URL_BASE}/auth/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
